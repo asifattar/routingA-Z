9eccaa41e05f7509a61fa09b322201901f54d56d
asif attar <asifattar3570@gmail.com>
2023-12-18 10:10:57 +0530

    routing is almost done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashbordComponent } from './shared/components/dashbord/dashbord.component';
+import { ProductsComponent } from './shared/components/products/products.component';
+import { UsersComponent } from './shared/components/users/users.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home', component: DashbordComponent
+  },
+  {
+    path: 'products', component: ProductsComponent
+  },
+  {
+    path: 'users', component: UsersComponent
+  },
+  {
+    path: '**', component: PageNotFoundComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { ProductsComponent } from './shared/components/products/products.component';
+import { UsersComponent } from './shared/components/users/users.component';
+import { DashbordComponent } from './shared/components/dashbord/dashbord.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductsComponent,
+    UsersComponent,
+    DashbordComponent,
+    FooterComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
