7bac9aca3244b58967e39a9a7a70f1a9e9940918
asif attar <asifattar3570@gmail.com>
2023-12-27 09:17:23 +0530

    workind on users components

---
+++ src/app/shared/interface/iusers.ts
@@ -0,0 +1,8 @@
+export interface iusers {
+    firstName: string;
+    lastName: string;
+    surname: string;
+    dateOfBirth: string;
+    nativePlace: string;
+    id: number
+}[]
---
+++ src/app/shared/services/users.service.ts
@@ -0,0 +1,98 @@
+import { Injectable } from '@angular/core';
+import { iusers } from '../interface/iusers';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+   usersArray : iusers[] = [
+    {
+      firstName: "John",
+      lastName: "Doe",
+      surname: "Smith",
+      dateOfBirth: "1990-05-15",
+      nativePlace: "New York ",
+      id : 1
+    },
+    {
+      firstName: "Alice",
+      lastName: "Johnson",
+      surname: "Brown",
+      dateOfBirth: "1988-09-23",
+      nativePlace: "Los Angeles",
+      id : 2},
+
+    {
+      firstName: "Michael",
+      lastName: "Williams",
+      surname: "Jones",
+      dateOfBirth: "1995-12-07",
+      nativePlace: "Chicago",
+      id : 3},
+
+    {
+      firstName: "Emily",
+      lastName: "Davis",
+      surname: "Wilson",
+      dateOfBirth: "1992-03-30",
+      nativePlace: "Houston" ,
+      id : 4},
+
+    {
+      firstName: "Daniel",
+      lastName: "Anderson",
+      surname: "Martinez",
+      dateOfBirth: "1985-11-10",
+      nativePlace: "Miami",
+      id : 5},
+
+    {
+      firstName: "Sophia",
+      lastName: "Garcia",
+      surname: "Thompson",
+      dateOfBirth: "1998-07-18",
+      nativePlace: "San Francisco",
+      id : 6},
+
+    {
+      firstName: "Matthew",
+      lastName: "Hernandez",
+      surname: "Lee",
+      dateOfBirth: "1993-01-25",
+      nativePlace: "Seattle",
+      id : 10},
+
+    {
+      firstName: "Olivia",
+      lastName: "Miller",
+      surname: "Walker",
+      dateOfBirth: "1997-06-08",
+      nativePlace: "Boston",
+      id : 7},
+
+    {
+      firstName: "David",
+      lastName: "Gonzalez",
+      surname: "Clark",
+      dateOfBirth: "1987-04-12",
+      nativePlace: "Dallas",
+      id : 8},
+
+    {
+      firstName: "Emma",
+      lastName: "Rodriguez",
+      surname: "Adams",
+      dateOfBirth: "1991-08-05",
+      nativePlace: "Phoenix",
+      id :9}
+
+  ];
+  
+
+  constructor() { }  
+
+  getAllusers() : Array<iusers>{
+    return this.usersArray
+  }
+}
